b35e052cf5a3951931a3352202c34596dc66aa5d
simran <simran@localhost>
2023-03-20 19:00:52 -0500

    Assignment1

---
+++ testProject/package1/Class1.java
@@ -0,0 +1,34 @@
+package package1;
+
+import java.util.Scanner;
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		int n1 = 225, n2 = 115, sum;  
+		sum = n1 + n2;  
+		System.out.println("The sum of numbers is: "+sum);  
+		
+		int n3;
+	      float n4;
+	      String a1;
+	      Scanner obj = new Scanner(System.in); /* 
+	 
+	      System.out.println("Enter a string:");
+	      a1 = obj.nextLine(); /* Take string input and assign to variable */
+	      System.out.println("You entered string "+a1); /* Print */
+	 
+	      System.out.println("Enter an integer:");
+	      n3 = obj.nextInt(); /* Take integer input and assign to variable */
+	      System.out.println("You entered integer "+n3); /* Print */
+	 
+	      System.out.println("Enter a float:");
+	      n4 = obj.nextFloat(); /* Take float input and assign to variable */
+	      System.out.println("You entered float "+n4); /* Print */
+
+	}
+
+}
+
